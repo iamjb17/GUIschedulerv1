f908d8450caa8e78d2900de8cbb5a5361a993836
Jessie <iamjb05@gmail.com>
2020-12-02 16:31:34 -0600

    Adding mysql connector

--- src/Scheduler/DBHelper.java
+++ src/Scheduler/DBHelper.java
@@ -3,9 +3,11 @@ package Scheduler;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class DBHelper {
     public static void getConnection() {
+        ResourceBundle reader = ResourceBundle.getBundle("dbconfig.properties");
         try (Connection connection = DriverManager.getConnection("jdbc:mysql://wgudb.ucertify.com:3306")) {
             System.out.println("connection successful");
         } catch (SQLException throwables) {
